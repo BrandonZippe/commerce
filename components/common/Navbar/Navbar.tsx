590325139e4bc7acd06f943ccf288d8e1fa5f713
Brandon Zippe <btzippe@gmail.com>
2021-04-12 10:27:08 -0500

    footer and nav mods

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -9,33 +9,33 @@ const Navbar: FC = () => (
   <NavbarRoot>
     <Container>
       <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
-        <div className="flex items-center flex-1">
+        <div className="flex items-center">
           <Link href="/">
             <a className={s.logo} aria-label="Logo">
               <Logo />
             </a>
           </Link>
-          <nav className="hidden ml-6 space-x-4 lg:block">
-            <Link href="/search">
-              <a className={s.link}>All</a>
+          <nav className="mx-6 space-x-4 lg:block flex-wrap">
+            <Link href="/about">
+              <a className={s.link}>About</a>
             </Link>
-            <Link href="/search?q=clothes">
-              <a className={s.link}>Clothes</a>
+            <Link href="/search?q=microphones">
+              <a className={s.link}>Microphones</a>
             </Link>
-            <Link href="/search?q=accessories">
-              <a className={s.link}>Accessories</a>
+            <Link href="/search?q=headphones">
+              <a className={s.link}>Headphones</a>
             </Link>
             <Link href="/search?q=shoes">
-              <a className={s.link}>Shoes</a>
+              <a className={s.link}>Media</a>
             </Link>
           </nav>
         </div>
 
-        <div className="justify-center flex-1 hidden lg:flex">
+        <div className="justify-center hidden lg:flex">
           <Searchbar />
         </div>
 
-        <div className="flex justify-end flex-1 space-x-8">
+        <div className="flex justify-end space-x-8">
           <UserNav />
         </div>
       </div>
