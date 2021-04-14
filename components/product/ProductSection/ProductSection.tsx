975d44fdf2687b69de19588d1a1c968f02c225bd
Brandon Zippe <BrandonZippe@users.noreply.github.com>
2021-04-14 08:29:22 -0500

    Revert "360 animation"
    This reverts commit afe42e2aa74cfbdfd022c6cb1f88fc6dd787bdfa.

--- components/product/ProductSection/ProductSection.tsx
+++ components/product/ProductSection/ProductSection.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react'
 import IndicatorLine from '../../icons/IndicatorLine'
-import { Controller, Scene } from 'react-scrollmagic'
 
 interface Props {
   className?: string
@@ -10,19 +9,15 @@ interface Props {
 
 const ProductSection: FC<Props> = ({ headline, copy, className }) => {
   return (
-    <Controller>
-      <Scene duration={900} pin>
-        <article
-          className={className + ' flex justify-start relative p-4 xl:my-20'}
-        >
-          <div className="content-wrap">
-            <h3 className="fancy text-3xl text-secondary">{headline}</h3>
-            <p className="text-primary">{copy}</p>
-          </div>
-          <IndicatorLine />
-        </article>
-      </Scene>
-    </Controller>
+    <article
+      className={className + ' flex justify-start relative p-4 xl:my-20'}
+    >
+      <div className="content-wrap">
+        <h3 className="fancy text-3xl text-secondary">{headline}</h3>
+        <p className="text-primary">{copy}</p>
+      </div>
+      <IndicatorLine />
+    </article>
   )
 }
 
