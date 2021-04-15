b4acc7ed289e6399b488fb5af21b0c73bee1e2ce
Brandon Zippe <BrandonZippe@users.noreply.github.com>
2021-04-15 10:20:42 -0500

    hide search input for now

--- components/common/Searchbar/Searchbar.tsx
+++ components/common/Searchbar/Searchbar.tsx
@@ -28,7 +28,7 @@ const Searchbar: FC<Props> = ({ className, id = 'search' }) => {
         </label>
         <input
           id={id}
-          className={s.input + ' border-2 rounded-full border-primary'}
+          className={s.input + ' border-2 rounded-full border-primary hidden'}
           placeholder="Search for products..."
           defaultValue={router.query.q}
           onKeyUp={(e) => {
