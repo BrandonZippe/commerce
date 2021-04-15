fe726f92cd75d992f4da25c85234c31234e38e4d
Brandon Zippe <BrandonZippe@users.noreply.github.com>
2021-04-15 12:20:12 -0500

    footer updates

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import type { Page } from '@framework/common/get-all-pages'
 import getSlug from '@lib/get-slug'
 import { Github, Vercel } from '@components/icons'
-import { Logo, Container } from '@components/ui'
+import { Logo, Container, Button, Input } from '@components/ui'
 import { I18nWidget } from '@components/common'
 import s from './Footer.module.css'
 
@@ -22,10 +22,10 @@ const Footer: FC<Props> = ({ className, pages }) => {
   const rootClassName = cn(className)
 
   return (
-    <footer className={rootClassName + ' bg-primary'}>
+    <footer className={rootClassName + ' bg-secondary-2'}>
       <Container>
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-accents-2 py-12 text-primary transition-colors duration-150">
-          <div className="col-span-1 lg:col-span-2">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-8 border-b border-primary py-12 text-primary-2 transition-colors duration-150">
+          {/* <div className="col-span-1 lg:col-span-2">
             <Link href="/">
               <a className="flex flex-initial items-center font-bold md:mr-24">
                 <span className="rounded-full border border-gray-700 mr-2">
@@ -33,9 +33,9 @@ const Footer: FC<Props> = ({ className, pages }) => {
                 </span>
               </a>
             </Link>
-          </div>
-          <div className="col-span-1 lg:col-span-2">
-            <ul className="flex flex-initial flex-col md:flex-1">
+          </div> */}
+          <div className="col-span-1 md:col-span-6 md:col-start-2">
+            <ul className="flex flex-row flex-wrap">
               {/* <li className="py-3 md:py-0 md:pb-4">
                 <Link href="/">
                   <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
@@ -50,17 +50,20 @@ const Footer: FC<Props> = ({ className, pages }) => {
                   </a>
                 </Link>
               </li> */}
-              <li className="py-3 md:py-0 md:pb-4">
+              <li className="py-2 md:py-0 w-full sm:w-1/3 md:pb-4">
                 <Link href="/blog">
-                  <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                  <a className="copy text-lg text-secondary hover:text-accents-6 transition ease-in-out duration-150">
                     Media
                   </a>
                 </Link>
               </li>
               {sitePages.map((page) => (
-                <li key={page.url} className="py-3 md:py-0 md:pb-4">
+                <li
+                  key={page.url}
+                  className="py-2 md:py-0 md:pb-4 w-full sm:w-1/3"
+                >
                   <Link href={page.url!}>
-                    <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
+                    <a className="copy text-lg text-secondary hover:text-accents-6 transition ease-in-out duration-150">
                       {page.name}
                     </a>
                   </Link>
@@ -68,18 +71,31 @@ const Footer: FC<Props> = ({ className, pages }) => {
               ))}
             </ul>
           </div>
-          <div className="col-span-1 lg:col-span-2">
-            <ul className="flex flex-initial flex-col md:flex-1">
-              {legalPages.map((page) => (
-                <li key={page.url} className="py-3 md:py-0 md:pb-4">
-                  <Link href={page.url!}>
-                    <a className="text-primary hover:text-accents-6 transition ease-in-out duration-150">
-                      {page.name}
-                    </a>
-                  </Link>
-                </li>
-              ))}
-            </ul>
+          <div className="col-span-1 md:col-span-3 md:col-start-9">
+            <div className="flex flex-col justify-center">
+              <div className="flex flex-row justify-start">
+                {/* <span className="bg-primary rounded-full h-10 w-10 mr-2">
+                  <Logo />
+                </span> */}
+                <h4 className="text-primary-2 subTitle text-xl mb-2">
+                  Email Signup
+                </h4>
+              </div>
+              <div className="signUp">
+                <Input
+                  className="mb-2 bg-primary-2 text-base"
+                  placeholder="Full Name"
+                />
+                <Input
+                  className="mb-2 bg-primary-2 text-base"
+                  type="email"
+                  placeholder="Email"
+                />
+                <Button type="submit" className="py-4 px-10">
+                  Submit
+                </Button>
+              </div>
+            </div>
           </div>
           {/* <div className="col-span-1 lg:col-span-6 flex items-start lg:justify-end text-primary">
             <div className="flex space-x-6 items-center h-10">
@@ -94,10 +110,22 @@ const Footer: FC<Props> = ({ className, pages }) => {
             </div>
           </div> */}
         </div>
-        <div className="py-12 flex flex-col md:flex-row justify-between items-center space-y-4">
+        <div className="py-4 flex flex-col md:flex-row justify-center items-center space-y-4 copy text-lg text-secondary">
+          <ul className="">
+            {legalPages.map((page) => (
+              <li key={page.url} className="py-3 md:py-0 md:pb-4">
+                <Link href={page.url!}>
+                  <a className="text-primary copy hover:text-accents-6 transition ease-in-out duration-150">
+                    {page.name}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
           <div>
             <span>&copy; 2021 512 Audio, Inc. All rights reserved.</span>
           </div>
+
           {/* <div className="flex items-center text-primary">
             <span className="text-primary">Crafted by</span>
             <a
